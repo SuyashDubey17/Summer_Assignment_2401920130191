0fb50568b54cca7eee91281fd9719834eb4affa6
Suyash Dubey <suyashdubey2217@gmail.com>
2026-06-01 22:53:49 +0530

    Create BestTimeToBuySellStock.java
    Day 1 - Stock problem solution

---
+++ Week_1/Day_1/BestTimeToBuySellStock.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int buyPrice = prices[0];
+        int bestProfit = 0;
+
+        for(int day = 1; day < prices.length; day++) {
+
+            int currentPrice = prices[day];
+
+            if(currentPrice < buyPrice) {
+                buyPrice = currentPrice; 
+            } 
+            else {
+                int profit = currentPrice - buyPrice;
+
+                if(profit > bestProfit) {
+                    bestProfit = profit;
+                }
+            }
+        }
+
+        return bestProfit;
+    }
+}
