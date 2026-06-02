4c5000dc873fea98bb95903cc5e15e40ca32002a
Suyash Dubey <suyashdubey2217@gmail.com>
2026-06-02 21:00:40 +0530

    Create ContainsDuplicate.java
    Day 2 - Contains Duplicate

---
+++ Week_1/Day_2/ContainsDuplicate.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean containsDuplicate(int[] arr) {
+        Arrays.sort(arr);
+        for(int i = 1; i < arr.length; i++) {
+
+            if(arr[i] == arr[i - 1]){
+                return true; 
+            }
+        }
+
+        return false;
+    }
+}
