5fd083eadd9b29b0fee1d110a29e992eb9b14a04
Suyash Dubey <suyashdubey2217@gmail.com>
2026-06-03 20:16:46 +0530

    Create MoveZeroes.java
    Day 3 - Move Zeroes

---
+++ Week_1/Day_3/MoveZeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] arr) {
+
+        int pos = 0; 
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != 0){
+                arr[pos] = arr[i];
+                pos++;
+            }
+        }
+        while(pos < arr.length){
+            arr[pos] = 0;
+            pos++;
+        }
+    }
+}
