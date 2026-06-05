747606fd53256cd4976e70ffd5fd4c6a26cf082b
Suyash Dubey <suyashdubey2217@gmail.com>
2026-06-05 12:27:25 +0530

    Day 5 - Valid Palindrome

---
+++ Week_1/Day_5/ValidPalindrome.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        int i = 0;
+        int j = s.length() - 1;
+
+        while(i < j){
+
+            if(!Character.isLetterOrDigit(s.charAt(i))){
+                i++;
+            }
+            else if(!Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+            }
+            else{
+                char a = Character.toLowerCase(s.charAt(i));
+                char b = Character.toLowerCase(s.charAt(j));
+
+                if(a != b){
+                    return false;
+                }
+
+                i++;
+                j--;
+            }
+        }
+
+        return true;
+    }
+}
