0b3f33529cce55844039fd0674ad4f33cc94c858
Suyash Dubey <suyashdubey2217@gmail.com>
2026-06-18 17:49:09 +0530

    Week 3 Day 4 - Largest Rectangle in Histogram

---
+++ Week_3/Day_4/LargestRectangle.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+
+        for(int i = 0; i <= heights.length; i++){
+
+            int h = (i == heights.length) ? 0 : heights[i];
+
+            while(!stack.isEmpty() && h < heights[stack.peek()]){
+
+                int height = heights[stack.pop()];
+
+                int width;
+
+                if(stack.isEmpty()){
+                    width = i;
+                } else {
+                    width = i - stack.peek() - 1;
+                }
+
+                int area = height * width;
+
+                if(area > maxArea){
+                    maxArea = area;
+                }
+            }
+
+            stack.push(i);
+        }
+
+        return maxArea;
+    }
+}
