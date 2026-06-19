c4bd9306fcc9eba41ae9f6e6525d616753e6ae3c
Suyash Dubey <suyashdubey2217@gmail.com>
2026-06-19 23:11:05 +0530

    Week 3 Day 5 - Sliding Window Maximum

---
+++ Week_3/Day_5/SlidingWindowMaximum.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+
+        Deque<Integer> dq = new LinkedList<>();
+        int[] result = new int[nums.length - k + 1];
+        int index = 0;
+
+        for(int i = 0; i < nums.length; i++){
+
+            while(!dq.isEmpty() && dq.peek() < i - k + 1){
+                dq.poll();
+            }
+
+            while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i]){
+                dq.pollLast();
+            }
+
+            dq.offer(i);
+
+            if(i >= k - 1){
+                result[index++] = nums[dq.peek()];
+            }
+        }
+
+        return result;
+    }
+}
