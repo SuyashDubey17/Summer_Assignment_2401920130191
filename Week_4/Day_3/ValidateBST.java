bfcacdde5054d05fa039dca6945ddc71dbce26d7
Suyash Dubey <suyashdubey2217@gmail.com>
2026-06-24 12:13:05 +0530

    Week 4 Day 3 - Validate Binary Search Tree

---
+++ Week_4/Day_3/ValidateBST.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return solve(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean solve(TreeNode root, long min, long max){
+
+        if(root == null){
+            return true;
+        }
+
+        if(root.val <= min || root.val >= max){
+            return false;
+        }
+
+        return solve(root.left, min, root.val) && solve(root.right, root.val, max);
+    }
+}
