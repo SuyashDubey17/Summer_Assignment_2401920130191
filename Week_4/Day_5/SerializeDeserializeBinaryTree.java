1ea658e283cb79b735fc909f95dd3f1e8df4325f
Suyash Dubey <suyashdubey2217@gmail.com>
2026-06-26 12:46:14 +0530

    Week 4 Day 5 - Serialize and Deserialize Binary Tree

---
+++ Week_4/Day_5/SerializeDeserializeBinaryTree.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Codec {
+
+    public String serialize(TreeNode root) {
+
+        StringBuilder sb = new StringBuilder();
+        helper(root, sb);
+        return sb.toString();
+    }
+
+    private void helper(TreeNode root, StringBuilder sb){
+
+        if(root == null){
+            sb.append("null,");
+            return;
+        }
+
+        sb.append(root.val).append(",");
+
+        helper(root.left, sb);
+        helper(root.right, sb);
+    }
+
+    public TreeNode deserialize(String data) {
+
+        Queue<String> queue = new LinkedList<>(Arrays.asList(data.split(",")));
+        return build(queue);
+    }
+
+    private TreeNode build(Queue<String> queue){
+
+        String value = queue.poll();
+
+        if(value.equals("null")){
+            return null;
+        }
+
+        TreeNode root = new TreeNode(Integer.parseInt(value));
+
+        root.left = build(queue);
+        root.right = build(queue);
+
+        return root;
+    }
+}
